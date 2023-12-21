c2e27ab7b2fb7893cb65cd3c75414c0b738b1541
Charles Annoni <charles@gdm-pixel.fr>
2023-12-21 13:30:34 +0100

    Fix Trailing Slash Breadcrumb, sitemap et robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  site: 'https://www.gdm-pixel.fr',
+  integrations: [tailwind(), react(), sitemap()]
 });
