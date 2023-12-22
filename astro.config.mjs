e8e8bb038aa90856dee3d281a1e68d39db38d3f9
Charles Annoni <charles@gdm-pixel.fr>
2023-12-22 15:44:49 +0100

    Test PartyTown GTM

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.gdm-pixel.fr',
-  integrations: [tailwind(), react(), sitemap()]
+  integrations: [tailwind(), react(), sitemap(), partytown({
+    // Adds dataLayer.push as a forwarding-event.
+    config: {
+      forward: ["dataLayer.push"],
+    },
+  })]
 });
